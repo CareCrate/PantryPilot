7008c57cb24ee1ca72856ec171a8fe76397a809b
Zarek Syed <zarek.syed@gmail.com>
2023-02-20 12:19:48 -0500

    Links on home page

--- carecrate/src/pages/index.tsx
+++ carecrate/src/pages/index.tsx
@@ -13,6 +13,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <a href="/volunteers/dashboard">Dashboard</a><br/>
+      <a href="/login">Login</a><br/>
+      <a href="/signup">Signup</a>
     </>
   )
 }
