fcd5a89969596300cbde1a58f0bd3a2f214bf301
Sanjay-B <sanjaybhadra1821@gmail.com>
2023-02-19 23:55:39 -0500

    Fixup! Footer changes.

--- carecrate/src/components/footer.tsx
+++ carecrate/src/components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <Paper component='footer' square variant='outlined' sx={{ marginTop: 'calc(10% + 60px)', width: '100%', position: 'relative', bottom: 0 }}>
             <Container maxWidth='lg'>
                 <Box sx={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center', display: 'flex', my: 2 }}>
-                    <Typography variant='caption' color='initial' align='center'>©{' '+new Date().getFullYear()+' CareCrate'}</Typography>
+                    <Typography variant='caption' color='initial' align='center'>©{' '+new Date().getFullYear()+' CareCrate. Made with ❤ to support the community.'}</Typography>
                 </Box>
             </Container>
         </Paper>
