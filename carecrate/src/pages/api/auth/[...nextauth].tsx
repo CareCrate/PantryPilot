94ce082536abccf309eaba9c6d5c07287e586621
Sanjay-B <sanjaybhadra1821@gmail.com>
2023-03-15 14:39:51 -0400

    Define Cert details as part of Firebase Auth.

--- carecrate/src/pages/api/auth/[...nextauth].tsx
+++ carecrate/src/pages/api/auth/[...nextauth].tsx
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider  from 'next-auth/providers/google';
 import { FirestoreAdapter } from '@next-auth/firebase-adapter';
+import { cert } from "firebase-admin/app"
 
 
 export default NextAuth({
@@ -11,7 +12,13 @@ export default NextAuth({
             allowDangerousEmailAccountLinking: true
         })
     ],
-    adapter: FirestoreAdapter(),
+    adapter: FirestoreAdapter({
+        credential: cert({
+            projectId: process.env.FIREBASE_AUTH_ADMIN_PROJECT_ID || '',
+            clientEmail: process.env.FIREBASE_AUTH_ADMIN_CLIENT_EMAIL || '',
+            privateKey: process.env.FIREBASE_AUTH_ADMIN_PRIVATE_KEY || ''
+        })
+    }),
     secret: process.env.secret,
     session: { strategy: 'jwt' }, 
     
