356e0986e638a80322a69346bbcc90cdb784257a
Sanjay-B <sanjaybhadra1821@gmail.com>
2023-04-03 02:04:43 -0400

    Feature! Remove Oauth, FirebaseAdapter. Move to Custom Provider (WIP).

--- carecrate/src/pages/api/auth/[...nextauth].tsx
+++ carecrate/src/pages/api/auth/[...nextauth].tsx
@@ -1,27 +1,37 @@
 import NextAuth from 'next-auth';
-import GoogleProvider  from 'next-auth/providers/google';
-import { FirestoreAdapter } from '@next-auth/firebase-adapter';
-import { cert } from "firebase-admin/app"
+import { User } from 'next-auth';
+import WorkspaceProvider from '@/providers/workspaceProvider';
+import { WorkspaceUser } from '@/types';
+import { AdapterUser } from 'next-auth/adapters';
 
 
 export default NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID || '',
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-            allowDangerousEmailAccountLinking: true
-        })
-    ],
-    adapter: FirestoreAdapter({
-        credential: cert({
-            projectId: process.env.FIREBASE_AUTH_ADMIN_PROJECT_ID || '',
-            clientEmail: process.env.FIREBASE_AUTH_ADMIN_CLIENT_EMAIL || '',
-            privateKey: process.env.FIREBASE_AUTH_ADMIN_PRIVATE_KEY || ''
-        })
-    }),
+    providers: [ WorkspaceProvider ],
     secret: process.env.secret,
     session: { strategy: 'jwt' }, 
     
     // Change this in production
-    debug: true
+    debug: true,
+
+    // Custom Provider Callbacks
+    callbacks: {
+        async jwt({ token, user }: { token: any; user: AdapterUser | User | undefined }) {
+            if (user && 'workspaceId' in user) {
+                token.id = user.id;
+                token.workspaceId = user.workspaceId;
+            }
+            return token;
+        },
+        async session({ session, user, token }) {
+            if (token && session) {
+                const { id, workspaceId } = token;
+                session.user = {
+                    ...user,
+                    id,
+                    workspaceId
+                };
+            }
+            return session;
+        }
+    }
 });
---
+++ carecrate/src/providers/workspaceProvider.ts
@@ -0,0 +1,60 @@
+import { CredentialsConfig } from 'next-auth/providers';
+import { User, RequestInternal } from 'next-auth';
+import { db } from '../firebase/initFirebase';
+import { collection , query, where, getDocs, doc } from 'firebase/firestore';
+import { WorkspaceUser, WorkspaceCredentials } from '@/types';
+
+const WorkspaceProvider: CredentialsConfig = {
+    id: "workspace",
+    name: "Workspace",
+    type: "credentials",
+    credentials: {
+        username: { label: "Username", type: "text", placeholder: "john.doe" },
+        password: { label: "Password", type: "password" },
+        workspace: { label: "Workspace", type: "text", placeholder: "myworkspace" },
+    },
+    authorize: async (credentials: Record<string, string> | undefined, req: Pick<RequestInternal, "body" | "query" | "headers" | "method">) => {
+        if (!credentials) {
+            return null;
+        }
+        const workspaceCredentials: WorkspaceCredentials = {
+            workspace: credentials.workspace,
+            username: credentials.username,
+            password: credentials.password
+        };
+        const user = await validateWorkspaceCredentials(workspaceCredentials);
+        if (user) {
+            return { ...user, account: { workspaceId: user.workspaceId }};
+        }
+        return Promise.resolve(null);
+    }
+};
+
+async function validateWorkspaceCredentials(credentials: WorkspaceCredentials): Promise<WorkspaceUser | null> {
+    try {
+        const { username, password, workspace } = credentials;
+        const workspaceSnapshot = await getDocs(query(collection(db, "workspaces"), where("name", "==", workspace)));
+        if (workspaceSnapshot.empty) {
+            return null;
+        }
+        const workspaceId = workspaceSnapshot.docs[0].id;
+        const userSnapshot = await getDocs(query(collection(doc(db, "worksapces", workspaceId), "users"), where("username", "==", username), where("password", "==", password)));
+        if (userSnapshot.empty) {
+            return null;
+        }
+        const userData = userSnapshot.docs[0].data();
+        const userId = userSnapshot.docs[0].id;
+        const user: WorkspaceUser = {
+            id: userId,
+            workspaceId: parseInt(workspaceId),
+            name: userData.name,
+            email: userData.email
+        }
+        return user;
+    } catch (error) {
+        console.error("Error validating workspace creds: ", error);
+        return null;
+    }
+}
+
+export default WorkspaceProvider;
