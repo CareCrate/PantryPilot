6cb78ea071a30262fcc0fc3a7289d3cf02685a54
Sanjay-B <sanjaybhadra1821@gmail.com>
2023-04-03 23:37:12 -0400

    Feature! Signout button in profile menu, session determines view.

--- carecrate/src/components/navbar.tsx
+++ carecrate/src/components/navbar.tsx
@@ -4,21 +4,21 @@ import AccountCircle from '@mui/icons-material/AccountCircle';
 import Brightness6Icon from '@mui/icons-material/Brightness6';
 import { ChangeEvent, useState } from "react";
 import { GetServerSideProps } from 'next';
-import type { Session } from 'next-auth';
-import { useSession, getSession } from 'next-auth/react';
+import { useSession, getSession, GetSessionParams, signOut } from 'next-auth/react';
+import { Session } from 'next-auth';
 
 export default function Navbar() {
-    const { data: session, status } = useSession()
-
-    // TODO: Replace with real Account Name
-    const ACCOUNT_NAME: string = 'John';
-
+    const { data: session, status } = useSession();
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
     const handleClose = () => {
         setAnchorEl(null);
     };
     const handleMenu = (event: ChangeEvent<HTMLElement>) => {
         setAnchorEl(event?.currentTarget);
+    };
+    const handleSignout = async () => {
+        await signOut({ callbackUrl: '/login' });
+        setAnchorEl(null);
     };
 
     const theme = useTheme();
@@ -33,27 +33,37 @@ export default function Navbar() {
     return (
         <AppBar position='static' elevation={0} sx={{ background: '#C2AFF0' }}>
             <Toolbar>
-                <Typography variant='h5' component='div' sx={{ flexGrow: 1, color: '#FFF' }}>Good Morning, {ACCOUNT_NAME}</Typography>
+                {session && (
+                    <Typography variant='h5' component='div' sx={{ flexGrow: 1, color: '#FFF' }}>Good Morning, {session?.user?.name}</Typography>
+                )}
+                {!session && (
+                    <Typography variant='h5' component='div' sx={{ flexGrow: 1, color: '#FFF' }}>CareCrate</Typography>
+                )}
                 <Box component='div'>
                     <Stack direction='row' sx={{ alignItems: 'center' }}>
-                        <IconButton size='large' disableRipple disableFocusRipple disableTouchRipple onClick={handleMenu}>
-                            <AccountCircle fontSize='large' sx={{ color: '#FFF' }}/>
-                        </IconButton>
+                        {session && (
+                            <IconButton size='large' disableRipple disableFocusRipple disableTouchRipple onClick={handleMenu}>
+                                <AccountCircle fontSize='large' sx={{ color: '#FFF' }}/>
+                            </IconButton>
+                        )}
                         <IconButton size='large' onClick={toggleMode}>
                             <Brightness6Icon fontSize='small' />
                         </IconButton>
                     </Stack>
-                    <Menu id='profile_menu' anchorEl={anchorEl} anchorOrigin={{ vertical: 'top', horizontal: 'right' }} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'right' }} open={Boolean(anchorEl)} onClose={handleClose} sx={{ marginTop: '2em', marginLeft: '-1em' }}>
-                        <MenuItem onClick={handleClose}>Settings</MenuItem>
-                        <MenuItem onClick={handleClose}>Logout</MenuItem>
-                    </Menu>
+                    {session && (
+                        <Menu id='profile_menu' anchorEl={anchorEl} anchorOrigin={{ vertical: 'top', horizontal: 'right' }} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'right' }} open={Boolean(anchorEl)} onClose={handleClose} sx={{ marginTop: '2em', marginLeft: '-1em' }}>
+                            {/* TODO: Implement Settings. */}
+                            {/* <MenuItem onClick={handleClose}>Settings</MenuItem> */}
+                            <MenuItem onClick={handleSignout}>Logout</MenuItem>
+                        </Menu>
+                    )}
                 </Box>
             </Toolbar>
         </AppBar>
     )
 }
 
-export const getServerSideProps: GetServerSideProps<{ session: Session | null }> = async (context) => {
+export const getServerSideProps: GetServerSideProps<{ session: Session | null }> = async (context: GetSessionParams | undefined) => {
     return {
       props: {
         session: await getSession(context)
