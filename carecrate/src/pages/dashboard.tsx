c6fb5cc7d09c4f4f5cde8ac5e92745c35ceff2ea
Sanjay-B <sanjaybhadra1821@gmail.com>
2023-04-03 21:26:51 -0400

    Feature! Implement session validation for Dashboard.

--- carecrate/src/pages/dashboard.tsx
+++ carecrate/src/pages/dashboard.tsx
@@ -2,7 +2,9 @@ import DataCard from '@/components/dashboard/DataCard';
 import Modal from '@/components/dashboard/Modal';
 import { Box, Button, Checkbox, FormControlLabel, FormGroup, Grid, Paper, Stack, TextField, Typography } from '@mui/material';
 import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 // Available Fields for Mapping
 const fields: GridColDef[] = [
@@ -68,47 +70,57 @@ const data: GridRowsProp = [
 ];
 
 export default function Dashboard() {
+    const router = useRouter();
+    const { data: session, status } = useSession();
     const [isCheckInModalOpen, setIsCheckInModalOpen] = useState(false)
     const handleAddCheckinClick = () => {
         setIsCheckInModalOpen(true);
     }
 
-    return (
-        <Box component='div' sx={{ overflowX: 'clip', position: 'relative', margin: 'auto', maxWidth: '1920px', padding: '2em' }}>
-            <Grid container spacing={0} direction='column' sx={{ width: '100%' }}>
+    useEffect(() => {
+        if (status === "unauthenticated" && !session) {
+            router.push('/login');
+        }
+    }, [status, session, router]);
 
-                <Grid item container direction='column' spacing={0} sx={{ flexDirection: 'column' }}>
+    if (session) {
+        return (
+            <Box component='div' sx={{ overflowX: 'clip', position: 'relative', margin: 'auto', maxWidth: '1920px', padding: '2em' }}>
+                <Grid container spacing={0} direction='column' sx={{ width: '100%' }}>
 
-                    {/* TODO: Implement Cards. */}
-                    <Stack direction='row' spacing={3}>
-                        <DataCard subtitle={'Total checkins today'} value={100} prev={120} />
-                        <DataCard subtitle={'Total volunteers today'} value={4} prev={20} />
-                        <DataCard subtitle={'Total household today'} value={3000} prev={2700} />
-                        {/* <DataCard subtitle={'Food weight'} value={25} units={'lbs'} /> */}
-                    </Stack>
+                    <Grid item container direction='column' spacing={0} sx={{ flexDirection: 'column' }}>
 
-                    {/* TODO: Implement Dynamic List */}
-                    <Stack direction='row' spacing={0} sx={{ marginTop: '5em' }}>
-                        <Typography variant='h6' sx={{ flexGrow: 1 }}>Recent Checkins</Typography>
-                        <Button variant='contained' disableElevation disableRipple disableTouchRipple sx={{ textTransform: 'none' }} onClick={handleAddCheckinClick}>+ Add Checkin</Button>
-                    </Stack>
-                    <Paper component='div' elevation={3} sx={{ height: 500, width: '100%', marginTop: '2em' }}>
-                        <DataGrid rows={data} columns={fields} />
-                    </Paper>
+                        {/* TODO: Implement Cards. */}
+                        <Stack direction='row' spacing={3}>
+                            <DataCard subtitle={'Total checkins today'} value={100} prev={120} />
+                            <DataCard subtitle={'Total volunteers today'} value={4} prev={20} />
+                            <DataCard subtitle={'Total household today'} value={3000} prev={2700} />
+                            {/* <DataCard subtitle={'Food weight'} value={25} units={'lbs'} /> */}
+                        </Stack>
+
+                        {/* TODO: Implement Dynamic List */}
+                        <Stack direction='row' spacing={0} sx={{ marginTop: '5em' }}>
+                            <Typography variant='h6' sx={{ flexGrow: 1 }}>Recent Checkins</Typography>
+                            <Button variant='contained' disableElevation disableRipple disableTouchRipple sx={{ textTransform: 'none' }} onClick={handleAddCheckinClick}>+ Add Checkin</Button>
+                        </Stack>
+                        <Paper component='div' elevation={3} sx={{ height: 500, width: '100%', marginTop: '2em' }}>
+                            <DataGrid rows={data} columns={fields} />
+                        </Paper>
+                    </Grid>
                 </Grid>
-            </Grid>
-            <Modal open={isCheckInModalOpen} onClose={() => setIsCheckInModalOpen(false)} title='Checkin' content='To checkin a user, please enter in their associated informaton. If a phone number is found, the information will be populated automatically.' submitText='Submit' inputFields={[
-                <TextField autoFocus margin="dense" id="phone_number" label="Phone Number" type="phone-number" fullWidth variant="standard" />,
-                <TextField autoFocus margin="dense" id="recipient" label="Recipient" type="recipien" fullWidth variant="standard" />,
-                <TextField autoFocus margin="dense" id="checkin_type" label="Type" type="checkin-type" fullWidth variant="standard" />,
-                <TextField autoFocus margin="dense" id="weight" label="Weight" type="weight" fullWidth variant="standard" />,
-                <TextField autoFocus margin="dense" id="first_name" label="First Name" type="first-name" fullWidth variant="standard" />,
-                <TextField autoFocus margin="dense" id="last_name" label="last Name" type="last-name" fullWidth variant="standard" />,
-                <TextField autoFocus margin="dense" id="number_in_household" label="# in Household" type="number-in-household" fullWidth variant="standard" />,
-                <TextField autoFocus margin="dense" id="number_under_18" label="# Under 18" type="number-under-18" fullWidth variant="standard" />,
-                <TextField autoFocus margin="dense" id="number_over_60" label="# Over 60" type="number-over-60" fullWidth variant="standard" />,
-                <FormGroup> <FormControlLabel control={<Checkbox defaultChecked={false} />} label="Add Associated Name to Phone Number" /></FormGroup>
-            ]}/>
-        </Box>
-    )
+                <Modal open={isCheckInModalOpen} onClose={() => setIsCheckInModalOpen(false)} title='Checkin' content='To checkin a user, please enter in their associated informaton. If a phone number is found, the information will be populated automatically.' submitText='Submit' inputFields={[
+                    <TextField autoFocus margin="dense" id="phone_number" label="Phone Number" type="phone-number" fullWidth variant="standard" />,
+                    <TextField autoFocus margin="dense" id="recipient" label="Recipient" type="recipien" fullWidth variant="standard" />,
+                    <TextField autoFocus margin="dense" id="checkin_type" label="Type" type="checkin-type" fullWidth variant="standard" />,
+                    <TextField autoFocus margin="dense" id="weight" label="Weight" type="weight" fullWidth variant="standard" />,
+                    <TextField autoFocus margin="dense" id="first_name" label="First Name" type="first-name" fullWidth variant="standard" />,
+                    <TextField autoFocus margin="dense" id="last_name" label="last Name" type="last-name" fullWidth variant="standard" />,
+                    <TextField autoFocus margin="dense" id="number_in_household" label="# in Household" type="number-in-household" fullWidth variant="standard" />,
+                    <TextField autoFocus margin="dense" id="number_under_18" label="# Under 18" type="number-under-18" fullWidth variant="standard" />,
+                    <TextField autoFocus margin="dense" id="number_over_60" label="# Over 60" type="number-over-60" fullWidth variant="standard" />,
+                    <FormGroup> <FormControlLabel control={<Checkbox defaultChecked={false} />} label="Add Associated Name to Phone Number" /></FormGroup>
+                ]}/>
+            </Box>
+        )
+    }
 }
