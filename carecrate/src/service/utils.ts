8c2dd6d58976f9c035847c31341dd2e3361890e9
Sanjay-B <sanjaybhadra1821@gmail.com>
2023-04-18 11:59:21 -0400

    Fixup! Auth utils.

--- carecrate/src/service/utils.ts
+++ carecrate/src/service/utils.ts
@@ -8,3 +8,24 @@ export function isWorkspaceUser(user: User | WorkspaceUser): user is WorkspaceUs
 export function isWorkspaceUserInSession(user: any): user is WorkspaceUser {
     return user && "workspaceId" in user;
 }
+
+export async function updateSession(user: Partial<WorkspaceUser>) {
+  const response = await fetch('/api/auth/update-session', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user),
+  });
+
+  if (!response.ok) {
+    const error = await response.json();
+    throw new Error(error.message || 'An error occurred while updating the session.');
+  }
+
+  return await response.json();
+}
+
+export async function saveSession(user: Partial<WorkspaceUser>) {
+  await updateSession(user);
+}
