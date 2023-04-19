cc39c35fc58bb1d0a77dbe10bcdb7e466d36bd8c
April Breedlove <abreed823@gmail.com>
2023-04-19 19:30:41 -0400

    Edit navbar text

--- carecrate/src/components/navbar.tsx
+++ carecrate/src/components/navbar.tsx
@@ -1,77 +1,81 @@
-import { useTheme } from '@mui/material/styles';
+import { useTheme } from "@mui/material/styles";
 import { AppBar, Box, Toolbar, Typography, IconButton, Menu, MenuItem, FormControlLabel, Stack, Switch } from "@mui/material";
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import Brightness6Icon from '@mui/icons-material/Brightness6';
-import TimelineIcon from '@mui/icons-material/Timeline';
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import Brightness6Icon from "@mui/icons-material/Brightness6";
+import TimelineIcon from "@mui/icons-material/Timeline";
 import { ChangeEvent, useState } from "react";
-import { GetServerSideProps } from 'next';
-import { useSession, getSession, GetSessionParams, signOut } from 'next-auth/react';
-import { Session } from 'next-auth';
-import { SessionUser } from '@/types';
+import { GetServerSideProps } from "next";
+import { useSession, getSession, GetSessionParams, signOut } from "next-auth/react";
+import { Session } from "next-auth";
+import { SessionUser } from "@/types";
 
 interface NavbarProps {
-    toggleMode: () => void;
-};
+  toggleMode: () => void;
+}
 
 export default function Navbar({ toggleMode }: NavbarProps) {
-    const { data: session, status } = useSession();
-    const user = session?.user as SessionUser | undefined;
-    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-    const handleMenu = (event: ChangeEvent<HTMLElement>) => {
-        setAnchorEl(event?.currentTarget);
-    };
-    const handleSignout = async () => {
-        await signOut({ callbackUrl: '/login' });
-        setAnchorEl(null);
-    };
+  const { data: session, status } = useSession();
+  const user = session?.user as SessionUser | undefined;
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleMenu = (event: ChangeEvent<HTMLElement>) => {
+    setAnchorEl(event?.currentTarget);
+  };
+  const handleSignout = async () => {
+    await signOut({ callbackUrl: "/login" });
+    setAnchorEl(null);
+  };
 
-    return (
-        <AppBar position='static' elevation={0} sx={{ background: '#C2AFF0' }}>
-            <Toolbar>
-                {session && (
-                    <Typography variant='h5' component='div' sx={{ flexGrow: 1, color: '#FFF' }}>Good Morning, {session?.user?.name}</Typography>
+  return (
+    <AppBar position="static" elevation={0} sx={{ background: "#C2AFF0" }}>
+      <Toolbar>
+        {session && (
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1, color: "#FFF" }}>
+            Hello, {session?.user?.name}
+          </Typography>
+        )}
+        {!session && (
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1, color: "#FFF" }}>
+            PantryPilot
+          </Typography>
+        )}
+        <Box component="div">
+          <Stack direction="row" sx={{ alignItems: "center" }}>
+            {session && (
+              <>
+                <IconButton size="large" disableRipple disableFocusRipple disableTouchRipple onClick={handleMenu}>
+                  <AccountCircle fontSize="large" sx={{ color: "#FFF" }} />
+                </IconButton>
+                {user?.role === "admin" && (
+                  <IconButton size="large" disableRipple disableFocusRipple disableTouchRipple>
+                    <TimelineIcon fontSize="large" sx={{ color: "#FFF" }} />
+                  </IconButton>
                 )}
-                {!session && (
-                    <Typography variant='h5' component='div' sx={{ flexGrow: 1, color: '#FFF' }}>CareCrate</Typography>
-                )}
-                <Box component='div'>
-                    <Stack direction='row' sx={{ alignItems: 'center' }}>
-                        {session && (
-                            <>
-                                <IconButton size='large' disableRipple disableFocusRipple disableTouchRipple onClick={handleMenu}>
-                                    <AccountCircle fontSize='large' sx={{ color: '#FFF' }}/>
-                                </IconButton>
-                                {(user?.role === 'admin') && (
-                                    <IconButton size='large' disableRipple disableFocusRipple disableTouchRipple>
-                                        <TimelineIcon fontSize='large' sx={{ color: '#FFF' }}/>
-                                    </IconButton>
-                                )}
-                            </>
-                        )}
-                        <IconButton size='large' onClick={toggleMode}>
-                            <Brightness6Icon fontSize='small' />
-                        </IconButton>
-                    </Stack>
-                    {session && (
-                        <Menu id='profile_menu' anchorEl={anchorEl} anchorOrigin={{ vertical: 'top', horizontal: 'right' }} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'right' }} open={Boolean(anchorEl)} onClose={handleClose} sx={{ marginTop: '2em', marginLeft: '-1em' }}>
-                            {/* TODO: Implement Settings. */}
-                            {/* <MenuItem onClick={handleClose}>Settings</MenuItem> */}
-                            <MenuItem onClick={handleSignout}>Logout</MenuItem>
-                        </Menu>
-                    )}
-                </Box>
-            </Toolbar>
-        </AppBar>
-    )
+              </>
+            )}
+            <IconButton size="large" onClick={toggleMode}>
+              <Brightness6Icon fontSize="small" />
+            </IconButton>
+          </Stack>
+          {session && (
+            <Menu id="profile_menu" anchorEl={anchorEl} anchorOrigin={{ vertical: "top", horizontal: "right" }} keepMounted transformOrigin={{ vertical: "top", horizontal: "right" }} open={Boolean(anchorEl)} onClose={handleClose} sx={{ marginTop: "2em", marginLeft: "-1em" }}>
+              {/* TODO: Implement Settings. */}
+              {/* <MenuItem onClick={handleClose}>Settings</MenuItem> */}
+              <MenuItem onClick={handleSignout}>Logout</MenuItem>
+            </Menu>
+          )}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export const getServerSideProps: GetServerSideProps<{ session: Session | null }> = async (context: GetSessionParams | undefined) => {
-    return {
-      props: {
-        session: await getSession(context)
-      }
+  return {
+    props: {
+      session: await getSession(context)
     }
-}
+  };
+};
