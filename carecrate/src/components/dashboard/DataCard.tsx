78f8f78b7887099836f4e454b69b877d71e444c0
April Breedlove <abreed823@gmail.com>
2023-04-27 15:15:28 -0400

    Cday changes

--- carecrate/src/components/dashboard/DataCard.tsx
+++ carecrate/src/components/dashboard/DataCard.tsx
@@ -14,10 +14,9 @@ type Props = {
   editTitle: string;
   editSubtext: string;
   editElements: JSX.Element[];
-  onSubmit: () => void;
 };
 
-const DataCard: FunctionComponent<Props> = ({ subtitle, value, prev, units, showPercent, editTitle, editSubtext, editElements, onSubmit }) => {
+const DataCard: FunctionComponent<Props> = ({ subtitle, value, prev, units, showPercent, editTitle, editSubtext, editElements }) => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const open = Boolean(anchorEl);
@@ -52,7 +51,20 @@ const DataCard: FunctionComponent<Props> = ({ subtitle, value, prev, units, show
         <MenuItem onClick={() => setIsModalOpen(true)}>Edit</MenuItem>
         {/* <MenuItem onClick={handleClose}>View Data</MenuItem> */}
       </Menu>
-      {(editTitle !== "" || editSubtext !== "" || editElements.length != 0) && <Modal open={isModalOpen} onClose={() => setIsModalOpen(false)} title={editTitle} content={editSubtext} submitText="Submit" inputFields={editElements} onSubmit={onSubmit} onCancel={() => setIsModalOpen(false)} />}
+      {(editTitle !== "" || editSubtext !== "" || editElements.length != 0) && (
+        <Modal
+          open={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          title={editTitle}
+          content={editSubtext}
+          submitText="Submit"
+          inputFields={editElements}
+          onSubmit={function (): void {
+            throw new Error("Function not implemented.");
+          }}
+          onCancel={() => setIsModalOpen(false)}
+        />
+      )}
     </Card>
   );
 };
